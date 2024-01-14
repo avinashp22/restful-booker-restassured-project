c2c9c084ba969e61dc6e24876fd587440eec6ea9
avinashp22 <avinashp22@gmail.com>
2024-01-14 11:24:20 +0000

    restful-booker-restassured-project

---
+++ src/test/java/com/restful/booker/testbase/TestBase.java
@@ -0,0 +1,15 @@
+package com.restful.booker.testbase;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+
+public class TestBase {
+
+    @BeforeClass
+    public void inIt() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        RestAssured.port = 8081;
+        RestAssured.basePath = "/booking";
+    }
+}
